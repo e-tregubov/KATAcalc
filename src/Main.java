c7c27ed76aa25e6484fb7545e042bec85e859954
jekuz <jekuz@mail.ru>
2022-09-01 13:23:14 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,33 @@
 import java.util.Scanner;
 
-public class Calculator {
-    static Scanner scanner = new Scanner(System.in);
-
+public class Main {
     public static void main(String[] args) {
-        int num1 = getInt();
-        int num2 = getInt();
-        char operation = getOperation();
-        int result = calc(num1,num2,operation);
-        System.out.println("Результат операции: "+result);
+        System.out.print("\nКалькулятор: ");
+        System.out.print("Результат: " + calc(new Scanner(System.in).nextLine()) + "\n");
     }
 
-    public static int getInt(){
-        System.out.println("Введите число:");
-        int num;
-        if(scanner.hasNextInt()){
-            num = scanner.nextInt();
-        } else {
-            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
-            scanner.next();//рекурсия
-            num = getInt();
-        }
-        return num;
+    public static String calc(String input) {
+        String[] part = input.split(" ");
+        if (part.length != 3) throw new IllegalArgumentException(input + " возможна только одна операция [+-*/]");
+        if (!part[1].matches("^[-+/*]$")) throw new IllegalArgumentException(input + " неправильная операция [+-*/]");
+
+        if (Numbers.isArabic(part[0]) && Numbers.isArabic(part[2]))
+            return "" + result(Integer.parseInt(part[0]), Integer.parseInt(part[2]), part[1]);
+
+        if (Numbers.isRoman(part[0]) && Numbers.isRoman(part[2]))
+            return Numbers.toRoman(result(Numbers.toArabic(part[0]), Numbers.toArabic(part[2]), part[1]));
+
+        else throw new IllegalArgumentException(input + " некорректный ввод чисел [1-10][I-X]");
     }
 
-    public static char getOperation(){
-        System.out.println("Введите операцию:");
-        char operation;
-        if(scanner.hasNext()){
-            operation = scanner.next().charAt(0);
-        } else {
-            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
-            scanner.next();//рекурсия
-            operation = getOperation();
-        }
-        return operation;
+    // возвращает результат операции двух чисел
+    private static int result(int num1, int num2, String action) {
+        return switch (action) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            default -> num1 / num2;
+        };
     }
 
-    public static int calc(int num1, int num2, char operation){
-        int result;
-        switch (operation){
-            case '+':
-                result = num1+num2;
-                break;
-            case '-':
-                result = num1-num2;
-                break;
-            case '*':
-                result = num1*num2;
-                break;
-            case '/':
-                result = num1/num2;
-                break;
-            default:
-                System.out.println("Операция не распознана. Повторите ввод.");
-                result = calc(num1, num2, getOperation());//рекурсия
-        }
-        return result;
-    }
 }
--- src/Numbers.java
+++ src/Numbers.java
@@ -3,43 +3,56 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-class Roman {
+class Numbers {
 
-    public static int toArabic(String input) {
-            String romanNumeral = input;
+    // проверяет диапазон арабского числа
+    static boolean isArabic(String string) { return string.matches("^[1-9]$|^10$"); }
+
+    // проверяет диапазон римского числа
+    static boolean isRoman(String string) { return string.matches("^IX$|^IV$|^V?I{0,3}$|^X$"); }
+
+    // возвращает арабское число из римского
+    public static int toArabic(String roman) {
             int result = 0;
-
             List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
-
             int i = 0;
 
-            while ((romanNumeral.length() > 0) && (i < romanNumerals.size())) {
+            while ((roman.length() > 0) && (i < romanNumerals.size())) {
                     RomanNumeral symbol = romanNumerals.get(i);
-                if (romanNumeral.startsWith(symbol.name())) {
+                if (roman.startsWith(symbol.name())) {
                     result += symbol.getValue();
-                    romanNumeral = romanNumeral.substring(symbol.name().length());
+                    roman = roman.substring(symbol.name().length());
                 }
                 else i++;
             }
 
-            if (romanNumeral.length() > 0) {
-            throw new IllegalArgumentException(input + " невозможно конвертировать в римские цифры");
-            }
-
+            if (roman.length() > 0) { throw new IllegalArgumentException(roman + " - невозможно конвертировать в римские цифры!"); }
             return result;
-            }
+    }
 
-    
+    // возвращает римское число из арабского
+    public static String toRoman(int number) {
+        if (number < 1) { throw new IllegalArgumentException(number + " - римское число не может быть меньше единицы!"); }
+        List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
 
+        while ((number > 0) && (i < romanNumerals.size())) {
+            RomanNumeral currentSymbol = romanNumerals.get(i);
+            if (currentSymbol.getValue() <= number) {
+                sb.append(currentSymbol.name());
+                number -= currentSymbol.getValue();
+            } else i++;
+        }
+        return sb.toString();
+    }
 
     enum RomanNumeral {
         I(1), IV(4), V(5), IX(9), X(10),
         XL(40), L(50), XC(90), C(100);
-        private int value;
+        private final int value;
         RomanNumeral(int value) { this.value = value; }
-
         public int getValue() { return value; }
-
         public static List<RomanNumeral> getReverseSortedValues() {
             return Arrays.stream(values())
                     .sorted(Comparator.comparing((RomanNumeral e) -> e.value).reversed())
